d7efb3923501bcf3c8077e74c13ebb6c1c552d53
황덕룡 <hwangsee12@naver.com>
2024-09-19 09:06:39 +0900

    1111

---
+++ src/views/Test.jsx
@@ -0,0 +1,14 @@
+//import 라이브러리
+import React, {useState, useContext } from 'react';
+
+const Test = () => {
+/*---상태관리 변수들(값이 변화면 화면 랜더링) ----------*/
+
+
+return (
+<>
+<h1>hello world</h1>
+</>
+);
+}
+export default Test;
